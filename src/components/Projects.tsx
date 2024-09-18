9f5c5a8f395c3bb5cd710b138d06a5bee27b8b68
samlaconico <99830026+samlaconico@users.noreply.github.com>
2024-09-17 19:47:13 -0700

    updated projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 import { projects } from "@/data";
 
 type projectProps = {
@@ -30,34 +30,44 @@ export function Projects() {
         {projects.map((data, index) => {
           if (index % 2 == 0) {
             return (
-              <div key={index} className="flex justify-start">
-                <Project
-                  key={index}
-                  body={data.body}
-                  image={data.image}
-                  tech={data.tech}
-                  techColors={data.techColors}
-                  link={data.link}
-                  reversed
-                >
-                  {data.title}
-                </Project>
+              <div className="">
+                <div key={index} className="flex justify-start">
+                  <Project
+                    key={index}
+                    body={data.body}
+                    image={data.image}
+                    tech={data.tech}
+                    techColors={data.techColors}
+                    link={data.link}
+                    reversed
+                  >
+                    {data.title}
+                  </Project>
+                </div>
+                <div className="select-none text-center font-extrabold">
+                  {index != projects.length - 1 ? "|" : ""}
+                </div>
               </div>
             );
           } else {
             return (
-              <div key={index} className="flex justify-end">
-                <Project
-                  key={index}
-                  body={data.body}
-                  image={data.image}
-                  tech={data.tech}
-                  techColors={data.techColors}
-                  link={data.link}
-                  reversed={false}
-                >
-                  {data.title}
-                </Project>
+              <div className="">
+                <div key={index} className="flex justify-end">
+                  <Project
+                    key={index}
+                    body={data.body}
+                    image={data.image}
+                    tech={data.tech}
+                    techColors={data.techColors}
+                    link={data.link}
+                    reversed={false}
+                  >
+                    {data.title}
+                  </Project>
+                </div>
+                <div className="select-none text-center font-extrabold">
+                  {index != projects.length - 1 ? "|" : ""}
+                </div>
               </div>
             );
           }
@@ -76,6 +86,8 @@ async function Project({
   link,
   reversed,
 }: projectProps) {
+  const [scope, animate] = useAnimate();
+
   if (reversed) {
     return (
       <>
@@ -90,11 +102,32 @@ async function Project({
 
             <div className="no-scrollbar flex flex-shrink gap-2 overflow-x-scroll">
               {tech.map((value, index) => {
-                  return <Tech key={index} color={techColors[index]}>{value}</Tech>;
+                return (
+                  <Tech key={index} color={techColors[index]}>
+                    {value}
+                  </Tech>
+                );
               })}
             </div>
           </div>
-          <a href={link}>
+          <a
+            ref={scope}
+            onMouseEnter={() => {
+              animate(
+                scope.current,
+                { rotate: -3, scale: 1.04 },
+                { duration: 0.2 },
+              );
+            }}
+            onMouseLeave={() => {
+              animate(
+                scope.current,
+                { rotate: 0, scale: 1 },
+                { duration: 0.2 },
+              );
+            }}
+            href={link}
+          >
             <img
               src={image}
               className="h-full w-full overflow-hidden object-cover"
@@ -107,7 +140,24 @@ async function Project({
     return (
       <>
         <motion.div className="my-8 grid h-[30rem] w-screen grid-cols-1 grid-rows-2 justify-between overflow-hidden rounded-xl border-[1px] bg-neutral-100 shadow-md md:h-96 md:w-5/6 md:grid-cols-2 md:grid-rows-1">
-          <a href={link}>
+          <a
+            ref={scope}
+            onMouseEnter={() => {
+              animate(
+                scope.current,
+                { rotate: 3, scale: 1.04 },
+                { duration: 0.2 },
+              );
+            }}
+            onMouseLeave={() => {
+              animate(
+                scope.current,
+                { rotate: 0, scale: 1 },
+                { duration: 0.2 },
+              );
+            }}
+            href={link}
+          >
             <img
               src={image}
               className="h-full w-full overflow-hidden object-cover"
@@ -123,7 +173,11 @@ async function Project({
 
             <div className="flex justify-end gap-2">
               {tech.map((value, index) => {
-                return <Tech key={index} color={techColors[index]}>{value}</Tech>;
+                return (
+                  <Tech key={index} color={techColors[index]}>
+                    {value}
+                  </Tech>
+                );
               })}
             </div>
           </div>
--- src/data/index.ts
+++ src/data/index.ts
@@ -8,6 +8,14 @@ export const projects = [
     link: "https://g1autoconcepts.com/",
   },
   {
+    title: "Genesis 1 Autoconcepts",
+    body: "Commercial small business automobile site, with a Markdown blog. Built using NextJ and Tailwind",
+    image: "images/g1.png",
+    tech: ["NextJS", "TailwindCSS", "Vercel", "Markdown"],
+    techColors: ["bg-superfami-green", "bg-superfami-green", "bg-superfami-blue", "bg-superfami-blue"],
+    link: "https://g1autoconcepts.com/",
+  },
+  {
     title: "Anime Theme Player",
     body: "Lightweight site to search and play a wide range of opening and endings from anime, old and new! Built using the Animethemes.moe API",
     image: "images/anithemes.png",
@@ -15,6 +23,7 @@ export const projects = [
     techColors: ["bg-superfami-green", "bg-superfami-green", "bg-superfami-blue"],
     link: "https://samlaconico.github.io/anithemes/",
   },
+  
 ];
 
 export const about = {
