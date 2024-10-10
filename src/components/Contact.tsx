cea80861f704cfbf4db2faf2dafbc2e24a1aba2f
samlaconico <99830026+samlaconico@users.noreply.github.com>
2024-10-10 09:22:57 -0700

    update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,12 @@
+import { motion } from "framer-motion";
+import { RiGithubFill, RiLinkedinBoxFill, RiMailLine } from "react-icons/ri";
+
+
 export function Contact() {
   return (
-    <div className="text-center">
+    <div className="text-center mb-24">
       <h1 className="font-manrope-bold text-4xl">Get connected with me!</h1>
-      <div className="my-16 font-manrope text-2xl">
+      <div className="my-8 font-manrope text-2xl">
         <p>
           Email:{" "}
           <span className="">
@@ -11,6 +15,39 @@ export function Contact() {
             </a>
           </span>
         </p>
+
+        <div className="flex w-full justify-center gap-4 m-auto text-center pt-2 text-4xl">
+            <motion.a
+              whileHover={{
+                scale: 1.3,
+                rotate: 3,
+                transition: { duration: 0.2 },
+              }}
+              href="https://github.com/samlaconico"
+            >
+              <RiGithubFill/>
+            </motion.a>
+            <motion.a
+              whileHover={{
+                scale: 1.3,
+                rotate: 3,
+                transition: { duration: 0.2 },
+              }}
+              href="https://www.linkedin.com/in/sam-laconico-971b70275/"
+            >
+              <RiLinkedinBoxFill />
+            </motion.a>
+            <motion.a
+              whileHover={{
+                scale: 1.3,
+                rotate: 3,
+                transition: { duration: 0.2 },
+              }}
+              href="https://github.com/samlaconico"
+            >
+              <RiMailLine/>
+            </motion.a>
+          </div>
       </div>
     </div>
   );
